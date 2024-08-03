6bff30db316e6e471f05c1ffc1e2571afedd080c
ynazaruk <nazaruk.ya@gmail.com>
2024-08-03 19:12:46 +0300

    9

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,10 +2,6 @@ import ImageCard from "../ImageCard/ImageCard";
 import css from "./ImageGallery.module.css"
 
 export default function ImageGallery({images, onImageClick}) {
-    if(images.length === 0) {
-        return null;
-    }
-    
     return (
         <ul className={css.gallery}>
         {images.map((image) =>
